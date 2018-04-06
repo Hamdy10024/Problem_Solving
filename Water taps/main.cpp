3ea8d111db9602234a318cdc38d95f434650c7a7
chalixar <hamdy10024@gmail.com>
2018-04-07 00:18:09 +0200

    water falls solved

--- Water taps/main.cpp
+++ Water taps/main.cpp
@@ -1,11 +1,67 @@
 #include <bits/stdc++.h>
 #define loop(k,n) for(int i=k;i<n;i++)
 #define pb(x) push_back(x)
-
 using namespace std;
 
+typedef pair<long long ,long long > ii;
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n,v;
+    cin>>n>>v;
+    ii poi[n];
+    loop(0,n)
+    {
+        cin>>poi[i].second;
+    }
+
+    loop(0,n)
+    {
+        cin>>poi[i].first;
+        poi[i].first-=v;
+    }
+    sort(poi,poi+n);
+    double all = 0,tot=0;
+    int i=0;
+    while(i < n && poi[i].first <= 0 )
+    {
+        all+=1.0*poi[i].first*poi[i].second;
+        tot+=poi[i].second;
+        i++;
+    }
+    while(i<n && all < 0)
+    {
+        double facr =1.0*poi[i].first*poi[i].second;
+        if(all+facr < 0 )
+        {
+            tot+=poi[i].second;
+            all+=facr;
+        }
+        else
+        {
+            double port = -1.0*all/poi[i].first;
+            all=0;
+            tot+=port;
+        }
+        i++;
+    }
+    i=0;
+    while (all<0)
+    {
+
+        double facr =1.0*poi[i].first*poi[i].second;
+        if(all - facr >0)
+        {
+            double port = all/poi[i].first;
+            all=0;
+            tot-=port;
+        }
+        else
+        {
+            all-=facr;
+            tot-=poi[i].second;
+        }
+        i++;
+    }
+    printf("%.9f",tot);
     return 0;
 }
